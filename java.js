b690e9799d3bbb30bff91beb10e22e5e42ae0351
AKOR35TH <merlynmajone@gmail.com>
2025-10-10 09:31:09 +0700

    Adicionar arquivos HTML e JS do projeto

---
+++ java.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript Basics</title>
+    <style>
+        span {
+            font-weight: bold;
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello there</h1>
+    <p>welcome. <span id="name">yourname</span></p>
+    <p> your address is <span id="address">your address</span></p>
+    <script src="app.js"></script>
+</body>
+</html>
